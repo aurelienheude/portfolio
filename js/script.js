4ec2a0e7793f3cd8a9fdc3334eafb4032091869f
Aurelien heude <aurelienheudepro@gmail.com>
2021-11-24 15:55:47 +0100

    go

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
-function wrapper_function() 
-{
-    var wrapper_menu_width = document.getElementById("wrapper_menu").offsetWidth;
-
-    if (wrapper_menu_width == 267)
-    {
-        wrapper_menu.style.width = "1px";
-        wrapper_menu.style.display = "none";
-    }
-    
-    else
-    {
-        wrapper_menu.style.width = "267px";
-        wrapper_menu.style.display = "flex";
-    }
-}
+$(function(){
+	
+	var note = $('#note'),
+		ts = new Date(2012, 0, 1),
+		newYear = true;
+	
+	if((new Date()) > ts){
+		// The new year is here! Count towards something else.
+		// Notice the *1000 at the end - time must be in milliseconds
+		ts = (new Date()).getTime() + 10*24*60*60*1000;
+		newYear = false;
+	}
+		
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+			message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+			
+			if(newYear){
+				message += "left until the new year!";
+			}
+			else {
+				message += "left to 10 days from now!";
+			}
+			
+			note.html(message);
+		}
+	});
+	
+});
